b6b5004060780b48e4c1e3bc1abb8036e071cfe6
buddhini.anuradha <buddhini.anuradha@mcmedisoft.com>
2021-08-10 00:32:03 +0530

    initial commit of angular commit

---
+++ web/wellfort/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wellfort';
+  obs: any;
+  userName: string="";
+  notification: any;
+ timeInterval=TIME_INTERVAL
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(){
+    
+    interval(this.timeInterval).subscribe(x =>{
+      this.http.get("http://localhost:8000/dashboard/test").subscribe((res)=>{this.notification=res;
+      console.log("notificaton",this.notification)
+    })
+    });
+  }
+}
+
+export interface Notification{
+  status:string
+  user:string
+  display_message:string
+}
+
+export const TIME_INTERVAL=10000
