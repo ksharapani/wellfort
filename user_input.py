b4abbc2d449296677545c069c3f08c0656b315c5
ksharapani-hrc <ksharapaniw@healthreconconnect.com>
2021-08-09 23:41:08 +0530

    add cros

--- user_input.py
+++ user_input.py
@@ -1,17 +1,26 @@
+
+
 from time import sleep
 import sqlite3
+
 
 from gpiozero import Button
 button_2 = Button(2)
 
-con = sqlite3.connect('db.sqlite3.db')
-cur = con.cursor()
+con = sqlite3.connect('db.sqlite3')
 
 
 while True:
     if button_2.wait_for_press():
-        for row in cur.execute('SELECT * FROM user where pin_number=1'):
-            print(row)
+        users = con.execute('SELECT id FROM dashboard_user where pin_number=2')
+        messages = con.execute('SELECT id FROM dashboard_message where message_id=1')
+
+        # messages = con.execute('INSERT INTO dashboard_queue(clicked_user, display_message, ) VALUES ({}, {]);'.
+        #                        format(users[0]))
+
+
+            # print(row[0])
         print('clicked')
 
     sleep(1)
+
--- wellfort/settings.py
+++ wellfort/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'dashboard'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
