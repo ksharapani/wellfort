c6c0d406a7e2bc367255dbee56acbe7b7f7c9c5c
ksharapani-hrc <ksharapaniw@healthreconconnect.com>
2021-08-10 09:04:49 +0530

    user input code

--- user_input.py
+++ user_input.py
@@ -2,7 +2,6 @@ from time import sleep
 from datetime import datetime
 import sqlite3
 
-
 from gpiozero import Button
 button_2 = Button(2)
 
@@ -18,11 +17,10 @@ while True:
         for row in con.execute('SELECT id FROM dashboard_message where message_id=1'):
             message = row[0]
 
-        print('INSERT INTO dashboard_queue(created_at, clicked_user_id, display_message_id, displayed) VALUES '
-              '("{}", {}, {}, 0);'.format(datetime.now(), user, message))
-        messages = con.execute('INSERT INTO dashboard_queue(created_at, clicked_user_id, display_message_id, '
-                               'displayed) VALUES ("{}", {}, {}, 0);'.format(datetime.now(), user, message))
+        con.execute('INSERT INTO dashboard_queue(created_at, clicked_user_id, display_message_id, displayed) '
+                    'VALUES ("{}", {}, {}, 0);'.format(datetime.now(), user, message))
+
+        con.commit()
         print('clicked')
 
     sleep(1)
-
