18dfb29d125694fd7acf601cffa3e5d55b6922a7
ksharapani <ksharapaniw@healthreconconnect.com>
2021-08-25 00:05:48 +0530

    UI changes

--- web/wellfort/src/app/app.component.ts
+++ web/wellfort/src/app/app.component.ts
@@ -9,32 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'wellfort';
   obs: any;
-  userName: string="";
+  userName: string = "";
   notification: any;
- timeInterval=TIME_INTERVAL
-  constructor(private http:HttpClient){
+  timeInterval = TIME_INTERVAL
+  constructor(private http: HttpClient) {
 
   }
-  ngOnInit(){
-    
-    interval(this.timeInterval).subscribe(x =>{
-      this.http.get("http://localhost:8000/dashboard/test").subscribe((res)=>{this.notification=res;
-      if(this.notification.audio!=null){
-        let audio = new Audio();
-        audio.src = "http://localhost:800/"+this.notification.audio;
-        audio.load();
-        audio.play();
-      }
- 
-    })
+  ngOnInit() {
+
+    interval(this.timeInterval).subscribe(x => {
+      this.http.get("http://localhost:8000/dashboard/test").subscribe((res) => {
+        this.notification = res;
+        if (this.notification.audio != null) {
+          let audio = new Audio();
+          audio.src = "http://localhost:800" + this.notification.audio;
+          audio.load();
+          audio.play();
+        }
+
+      })
     });
   }
 }
 
-export interface Notification{
-  status:string
-  user:string
-  display_message:string
+export interface Notification {
+  status: string
+  user: string
+  display_message: string
 }
 
-export const TIME_INTERVAL=10000
+export const TIME_INTERVAL = 10000
