37378914a33bf02d1c7e0b25753a7220f8e8066f
ksharapani <ksharapaniw@healthreconconnect.com>
2021-08-29 08:33:47 +0530

    add db

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,12 +7,18 @@ class User(models.Model):
     name = models.CharField(max_length=512)
     pin_number = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     objects = None
 
     message_id = models.IntegerField()
     message = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.message
 
 
 class Queue(models.Model):
@@ -22,3 +28,6 @@ class Queue(models.Model):
     display_message = models.ForeignKey(Message, on_delete=models.CASCADE)
     displayed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return str(self.created_at)
