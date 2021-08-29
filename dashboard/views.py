25d0e66da113f9564120e4cfda6b28c696978216
ksharapani <ksharapaniw@healthreconconnect.com>
2021-08-29 12:42:21 +0530

    change audio logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from gtts import gTTS
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.db.models import Count, F
@@ -10,6 +11,7 @@ class Dashboard(APIView):
 
     def get(self, request):
         today = datetime.today()
+        language = 'en'
         data = Queue.objects.filter(displayed=False).order_by('created_at').first()
 
         queue_data = Queue.objects.filter(created_at__year=today.year, created_at__month=today.month,
@@ -18,9 +20,12 @@ class Dashboard(APIView):
 
         output = {'status': 'failed', 'user': None, 'display_message': None, 'audio': None, 'data': queue_data}
         if data:
+            audio = gTTS(text='{} {}'.format(data.display_message.message, data.clicked_user.name), lang=language, slow=False)
+            audio.save("static/audio/message.mp3")
+
             output = {'user': data.clicked_user.name, 'status': 'successful',
                       'display_message': data.display_message.message,
-                      'audio': 'static/audio/{}.mp3'.format(data.display_message.id),
+                      'audio': 'static/audio/message.mp3',
                       'data': queue_data}
             data.displayed = True
             data.save()
