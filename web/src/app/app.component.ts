c3c6cb88ea32af623d95716aff8ab4ee4583ff75
ksharapani <ksharapaniw@healthreconconnect.com>
2021-08-29 12:29:29 +0530

    change folder

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wellfort';
+  obs: any;
+  userName: string = "";
+  notification: any;
+  timeInterval = TIME_INTERVAL
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+
+    interval(this.timeInterval).subscribe(x => {
+      this.http.get("http://localhost:8000/dashboard/test").subscribe((res) => {
+        this.notification = res;
+        if (this.notification.audio != null) {
+          let audio = new Audio();
+         audio.src = "http://localhost:8000/" + this.notification.audio;
+ 
+          audio.load();
+          audio.play();
+          console.log("audio playing...")
+        }
+      })
+    });
+  }
+}
+
+export interface Notification {
+  status: string
+  user: string
+  display_message: string
+}
+
+export const TIME_INTERVAL = 10000
---
+++ web/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BubblingTextDirective } from "./bubbling-text.directive";
+@NgModule({
+  declarations: [
+    AppComponent, BubblingTextDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
